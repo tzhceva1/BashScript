f7a62237297b2039f761911e5509e7c91a7ec6c4
Censori Valerio <Valerio.Censori@swisscom.com>
2020-10-09 04:58:53 +0200

    first commit

---
+++ ping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#########################
+# author/copyright: TEST
+#########################
+
+
+# USAGE, dry_run, INCL="^", EXCL="a^", are defaults from the function file.
+function_file="generic_functions.sh"
+
+# Default IP list:
+IP_LIST="${CONF_DIR}/full_cmu_csu_macip_list.conf"
+
